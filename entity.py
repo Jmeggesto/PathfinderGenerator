1d1d9501f913b85e7ded28a91b625b777149f164
Jackie Meggesto <jackie.meggesto@gmail.com>
2015-09-23 16:46:50 -0400

    commit

--- entity.py
+++ entity.py
@@ -1,33 +1,32 @@
 import random
 
-
 def fourd6_drop_lowest():
-    rolls = []
-    [rolls.append(random.randint(1, 6)) for i in range(1, 5)]
-    return sum([roll for roll in rolls if roll is not min(rolls)])
+    rolls = [random.randint(1, 6) for i in range(4)]
+    return sum(sorted(rolls)[1:])
 
 def random_abilities(base = None):
     if base is None:
         base = {ability : 0 for ability in ["Str", "Dex", "Con", "Int", "Wis", "Cha"]}
 
     for ability in base:
-        base[ability] = fourd6_drop_lowest()
+        base[ability] += fourd6_drop_lowest()
+
     return base
 
 class Entity():
-	def __init__(self,
-				 name = '',
-				 hp = 0,
-				 armor_class = 0,
-				 abilities = {},
-				 ability_mods = {},
-				 base_attack_bonus = 0,
-				 damage = 0
-				 ):
-		self.name = name
-		self.hp = hp
-		self.abilities = random_abilities()
-		self.ability_mods = {ability : int(self.abilities[ability] / 2 - 5) for ability in self.abilities}
-		self.base_attack_bonus = base_attack_bonus
-		self.damage = damage
+
+    def __init__(self,
+                name = '',
+                hp = 0, 
+                base_attack_bonus = 0,
+                damage = 0
+            ):
+        self.name = name
+        self.hp = hp
+        self.abilities = random_abilities()
+        self.ability_mods = {ability : score // 2 - 5 for ability, score in self.abilities.items()}
+        self.base_attack_bonus = base_attack_bonus
+        self.damage = damage
+    	
+    	
 
