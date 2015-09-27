01bfcd8de495e82e937505019bcd011f559bfb56
Max White <maxjwhite22@gmail.com>
2015-09-27 14:24:39 -0400

    refactored entity class so attribute generation functions are class methods

--- entity.py
+++ entity.py
@@ -1,32 +1,23 @@
 import random
+class Entity(object):
+    def __init__(self, **kwargs):
+        self.name = kwargs.get('name')
+        self.set_random_abilities()
+        self.klass = kwargs.get('class')
+        self.hp = 10
+        self.base_attack_bonus = 2
+        self.damage = 2
 
-def fourd6_drop_lowest():
-    rolls = [random.randint(1, 6) for i in range(4)]
-    return sum(sorted(rolls)[1:])
-
-def random_abilities(base = None):
-    if base is None:
-        base = {ability : 0 for ability in ["Str", "Dex", "Con", "Int", "Wis", "Cha"]}
-
-    for ability in base:
-        base[ability] += fourd6_drop_lowest()
-
-    return base
-
-class Entity():
-
-    def __init__(self,
-                name = '',
-                hp = 0, 
-                base_attack_bonus = 0,
-                damage = 0
-            ):
-        self.name = name
-        self.hp = hp
-        self.abilities = random_abilities()
+    # generate random ability scores
+    def fourd6_drop_lowest(self):
+        rolls = [random.randint(1, 6) for i in range(4)]
+        return sum(sorted(rolls)[1:])
+    def set_random_abilities(self, base = None):
+        if base is None:
+            base = {ability : 0 for ability in ["Str", "Dex", "Con", "Int", "Wis", "Cha"]}
+        for ability in base:
+            base[ability] += self.fourd6_drop_lowest()
+        self.abilities = base
+        self.updateMods()
+    def updateMods(self):
         self.ability_mods = {ability : score // 2 - 5 for ability, score in self.abilities.items()}
-        self.base_attack_bonus = base_attack_bonus
-        self.damage = damage
-    	
-    	
-
